eb3152835ab432b7dcc396af30a0ab39f4a279f4
Edmundo Pichardo <ce.pichardo@gmail.com>
2019-10-23 07:39:38 -0400

    Create Category with Sluggable

---
+++ app/Category.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use Cviebrock\EloquentSluggable\Sluggable;
+
+class Category extends Model
+{
+    use Sluggable;
+
+
+    protected $fillable = ['name','slug'];
+
+    /**
+     * Return the sluggable configuration array for this model.
+     *
+     * @return array
+     */
+    public function sluggable()
+    {
+        return [
+            'slug' => [
+                'source' => 'name'
+            ]
+        ];
+    }
+
+}
