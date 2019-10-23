e3fd2711fab1f6f0d03df15884c78d7c1cf21794
Edmundo Pichardo <ce.pichardo@gmail.com>
2019-10-23 07:23:55 -0400

    Start Repo

---
+++ database/migrations/2019_10_23_111137_create_people_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePeopleTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('people', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->bigInteger('user_id')->unsigned();
+            $table->string('first');
+            $table->string('last')->nullable();
+            $table->date('birth_date')->nullable();
+            $table->enum('gender',['male','female'])->nullable();
+
+
+            $table->timestamps();
+
+            $table->foreign('user_id')
+                ->references('id')->on('users')
+                ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('people');
+    }
+}
